d34a85ee1ca536bb686bac930cc69c0cb9237d67
Roey Honig <roeyhonig94@gmail.com>
2022-01-08 22:15:24 +0200

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,3 +16,27 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/** how to use firebase SDK
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAWZO70q5GI-VWaC3jSoqQLehEdYwKqp1U",
+  authDomain: "recipes-management-console.firebaseapp.com",
+  projectId: "recipes-management-console",
+  storageBucket: "recipes-management-console.appspot.com",
+  messagingSenderId: "702983449327",
+  appId: "1:702983449327:web:234de27201893751a9c91a",
+  measurementId: "G-V08PV6L961"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+ */
