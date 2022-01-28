3bed721ecc40334a50181768ec4cbd1742ad7a15
roeyHonig <37895849+roeyHonig@users.noreply.github.com>
2022-01-29 01:40:45 +0200

    Add new pages and setup routes (#2)
    * Add page components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPageComponent } from './add-page/add-page.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/listings',
+    pathMatch: 'full'
+  },
+  {
+    path: 'listings',
+    component: ListingsPageComponent,
+  },
+  {
+    path: 'details/:id',
+    component: DetailsPageComponent,
+  },
+  {
+    path: 'edit/:id',
+    component: EditPageComponent,
+  },
+  {
+    path: 'add',
+    component: AddPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
